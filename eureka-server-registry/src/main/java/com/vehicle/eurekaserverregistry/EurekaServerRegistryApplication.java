5c9a7274fd12f0c6e04d6012473b768e75ab30bb
pnabee <40293573+pnabee@users.noreply.github.com>
2021-05-06 02:18:38 +0530

    Add files via upload

---
+++ eureka-server-registry/src/main/java/com/vehicle/eurekaserverregistry/EurekaServerRegistryApplication.java
@@ -0,0 +1,17 @@
+package com.vehicle.eurekaserverregistry;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerRegistryApplication.class, args);
+	}
+
+}

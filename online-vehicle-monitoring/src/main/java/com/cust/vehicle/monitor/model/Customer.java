a67a38bf3f93c3509afe0ae1dce0d75cfde720e3
pnabee <40293573+pnabee@users.noreply.github.com>
2021-05-15 03:35:30 +0530

    Update Customer.java

--- online-vehicle-monitoring/src/main/java/com/cust/vehicle/monitor/model/Customer.java
+++ online-vehicle-monitoring/src/main/java/com/cust/vehicle/monitor/model/Customer.java
@@ -2,20 +2,14 @@ package com.cust.vehicle.monitor.model;
 
 import java.io.Serializable;
 import java.util.HashSet;
-
-
 import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.Valid;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;

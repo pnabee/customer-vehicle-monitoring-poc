6d63072bdb339c5cea03b7669bef8e94ce8a4898
pnabee <40293573+pnabee@users.noreply.github.com>
2021-05-15 03:38:51 +0530

    Update VehicleRepository.java

--- online-vehicle-monitoring/src/main/java/com/cust/vehicle/monitor/repo/VehicleRepository.java
+++ online-vehicle-monitoring/src/main/java/com/cust/vehicle/monitor/repo/VehicleRepository.java
@@ -1,8 +1,7 @@
 package com.cust.vehicle.monitor.repo;
 
-import java.util.Collection;
-import java.util.List;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,9 +16,8 @@ import com.cust.vehicle.monitor.model.Vehicle;
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 	
-	@Query(value = "SELECT * FROM vehicle WHERE name=?",  nativeQuery = true)
-	List<Vehicle> findByVehicleWithName(@Param("name") String name);
-	//Collection<Vehicle> findByVehicleWithName(@Param("name") String name);
+	@Query(value = "SELECT * FROM vehicle WHERE custid=?",  nativeQuery = true)
+	List<Vehicle> findByVehiclesByCustId(@Param("custid") String custid);
 	
 	
 
